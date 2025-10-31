38094e42e086faf10528ebedb0e6879db75b2db9
Annie Huang <annie.hn@gmail.com>
2025-10-31 17:22:20 +1100

    Add resolver into useForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import {useForm} from 'react-hook-form';
 import z from 'zod';
 import {projectSchema} from '@/schemas/project';
+import {zodResolver} from '@hookform/resolvers/zod';
 
 export default function Home() {
-  useForm<z.infer<typeof projectSchema>>({
+  const form = useForm<z.infer<typeof projectSchema>>({
     defaultValues: {
       name: ''
-    }
+    },
+    resolver: zodResolver(projectSchema)
   })
 
   return null;
