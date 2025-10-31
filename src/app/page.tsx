8f6f7df8b8caac0e225ab30fda757f5bf17e8240
Annie Huang <annie.hn@gmail.com>
2025-10-31 17:18:50 +1100

    Add useForm from react-hook-form in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 'use client'
 
+import {useForm} from 'react-hook-form';
+import z from 'zod';
+import {projectSchema} from '@/schemas/project';
+
 export default function Home() {
+  useForm<z.infer<typeof projectSchema>>({
+    defaultValues: {
+      name: ''
+    }
+  })
+
   return null;
 }
