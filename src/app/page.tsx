cbe628aacdf2111cecf604eebe40c7e48937360b
Annie Huang <annie.hn@gmail.com>
2025-11-01 13:51:12 +1100

    call createProject in onSubmit method.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
-'use client'
+'use client';
 
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import z from 'zod';
-import {projectSchema} from '@/schemas/project';
-import {zodResolver} from '@hookform/resolvers/zod';
+import { projectSchema } from '@/schemas/project';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import { createProject } from '@/actions/project';
 
 export default function Home() {
   const form = useForm<z.infer<typeof projectSchema>>({
     defaultValues: {
-      name: ''
+      name: '',
     },
-    resolver: zodResolver(projectSchema)
-  })
+    resolver: zodResolver(projectSchema),
+  });
 
-  return null;
+  const onSubmit = async (data: z.infer<typeof projectSchema>) => {
+    const res = await createProject(data);
+
+    if (res.success) {
+      form.reset();
+      toast.success('Project created successfully!', {
+        description: JSON.stringify(data, null, 2),
+        className: 'whitespace-pre-wrap font-mono',
+      });
+    } else {
+      toast.error('Failed to create project.');
+    }
+  };
+
+  return <form onSubmit={form.handleSubmit(onSubmit)}></form>;
 }
