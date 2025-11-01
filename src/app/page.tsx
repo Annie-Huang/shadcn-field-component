2cda1c9b4649e866f73c9e9438bfd984215ffafb
Annie Huang <annie.hn@gmail.com>
2025-11-01 15:58:39 +1100

    Add Field Group, Field, FieldLabel, Input and FieldError into the template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,13 @@ import { projectSchema } from '@/schemas/project';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
 import { createProject } from '@/actions/project';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
   const form = useForm<z.infer<typeof projectSchema>>({
@@ -31,7 +38,15 @@ export default function Home() {
 
   return (
     <div className='container px-4 mx-auto my-6'>
-      <form onSubmit={form.handleSubmit(onSubmit)}></form>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FieldGroup>
+          <Field>
+            <FieldLabel>Name</FieldLabel>
+            <Input />
+            <FieldError errors={[{ message: 'Hi' }]} />
+          </Field>
+        </FieldGroup>
+      </form>
     </div>
   );
 }
