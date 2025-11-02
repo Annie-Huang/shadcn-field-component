040ab353fe2ec8226b4e3e27070b64e36b5857b8
Annie Huang <annie.hn@gmail.com>
2025-11-02 17:01:47 +1100

    Update projectSchme to pass the typescript check in page.tsx

--- src/schemas/project.ts
+++ src/schemas/project.ts
@@ -5,5 +5,8 @@ export const PROJECT_STATUSES = ['draft', 'active', 'finished'] as const;
 export const projectSchema = z.object({
   name: z.string().min(1),
   status: z.enum(PROJECT_STATUSES),
-  description: z.string().transform((v) => v || undefined),
+  description: z
+    .string()
+    .transform((v) => v || undefined)
+    .optional(), // Add optional otherwise the page.tsx will not pass typescript as here only state the value can be string only.
 });
