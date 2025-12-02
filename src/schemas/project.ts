6a2e12a548edd4966994a8454bd2779fe78ecc92
Annie Huang <annie.hn@gmail.com>
2025-12-02 17:24:34 +1100

    Update projectSchema as well

--- src/schemas/project.ts
+++ src/schemas/project.ts
@@ -2,6 +2,7 @@ import z from 'zod';
 
 export const PROJECT_STATUSES = ['draft', 'active', 'finished'] as const;
 
+/*// For ReactHookForm
 export const projectSchema = z.object({
   name: z.string().min(1),
   status: z.enum(PROJECT_STATUSES),
@@ -9,6 +10,22 @@ export const projectSchema = z.object({
     .string()
     .transform((v) => v || undefined)
     .optional(), // Add optional otherwise the page.tsx will not pass typescript as here only state the value can be string only.
+  notifications: z.object({
+    email: z.boolean(),
+    sms: z.boolean(),
+    push: z.boolean(),
+  }),
+  users: z
+    .array(z.object({ email: z.email() }))
+    .min(1)
+    .max(5),
+});*/
+
+// For tanstack form
+export const projectSchema = z.object({
+  name: z.string().min(1),
+  status: z.enum(PROJECT_STATUSES),
+  description: z.string().transform((v) => v || undefined),
   notifications: z.object({
     email: z.boolean(),
     sms: z.boolean(),
